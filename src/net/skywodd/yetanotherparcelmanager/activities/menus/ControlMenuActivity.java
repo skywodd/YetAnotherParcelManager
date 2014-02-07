20140fe21d8631d528a4f3a01c355a59e50fd99a
Pireyre Dimitri <dimitripireyre63@gmail.com>
2014-02-07 17:05:40 +0100

    Add Menu activities

--- src/net/skywodd/yetanotherparcelmanager/activities/menus/ControlMenuActivity.java
+++ src/net/skywodd/yetanotherparcelmanager/activities/menus/ControlMenuActivity.java
@@ -1,7 +1,44 @@
+/*
+ *  This file is part of YetAnotherParcelManager.
+ *
+ *   YetAnotherParcelManager is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   YetAnotherParcelManager is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with YetAnotherParcelManager. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.skywodd.yetanotherparcelmanager.activities.menus;
 
+import android.os.Bundle;
+import net.skywodd.yetanotherparcelmanager.R;
 import net.skywodd.yetanotherparcelmanager.activities.AbstractBaseActivity;
 
+/**
+ * This class manage ControlMenuActivity.
+ * 
+ * @author Linkdelaudela
+ * @version 1.0.0
+ */
 public class ControlMenuActivity extends AbstractBaseActivity {
 
+	
+	/*
+	 * (non-Javadoc)
+	 * @see net.skywodd.yetanotherparcelmanager.activities.AbstractBaseActivity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_control);
+		
+	}
 }
