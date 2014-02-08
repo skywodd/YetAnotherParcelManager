91db94306a41c27a42d1d44c218eb0213f720284
Skywodd <skywodd@gmail.com>
2014-02-08 09:06:11 +0100

    Cleanup

--- src/net/skywodd/yetanotherparcelmanager/activities/AbstractBaseActivity.java
+++ src/net/skywodd/yetanotherparcelmanager/activities/AbstractBaseActivity.java
@@ -14,17 +14,11 @@
  *   You should have received a copy of the GNU General Public License
  *   along with YetAnotherParcelManager. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package net.skywodd.yetanotherparcelmanager.activities;
 
 import net.skywodd.yetanotherparcelmanager.R;
-import net.skywodd.yetanotherparcelmanager.activities.menus.CommandMenuActivity;
-import net.skywodd.yetanotherparcelmanager.activities.menus.ControlMenuActivity;
-import net.skywodd.yetanotherparcelmanager.activities.menus.ConverterMenuActivity;
-import net.skywodd.yetanotherparcelmanager.activities.parcels.ListParcelActivity;
 import net.skywodd.yetanotherparcelmanager.helpers.DatabaseHelper;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -32,120 +26,58 @@ import android.view.MenuItem;
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 
 /**
- * This class is the base for the creation of every other activity, 
- * she implement the creation of an option menu which will be used in other activties
+ * Base class for all activities with ORM dependency and option menu.
  * 
  * @author Linkdelaudela
  * @version 1.0.0
  */
-
 public class AbstractBaseActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 
 	/*
 	 * (non-Javadoc)
-	 * @see com.j256.ormlite.android.apptools.OrmLiteBaseActivity#onCreate(android.os.Bundle)
-	 */
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		
-	}
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onStart()
-	 */
-	@Override
-	protected void onStart() {
-		super.onStart();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onRestart()
-	 */
-	@Override
-	protected void onRestart() {
-		super.onRestart();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onResume()
-	 */
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onPause()
-	 */
-	@Override
-	protected void onPause() {
-		super.onPause();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onStop()
-	 */
-	@Override
-	protected void onStop() {
-		super.onStop();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.j256.ormlite.android.apptools.OrmLiteBaseActivity#onDestroy()
-	 */
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-	}
-
-	/*
-	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
 	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
-		MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.menu_main, menu);
-		return true;
 
+		// Inflate the default menu.
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_main, menu);
+
+		// Menu created.
+		return true;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
 	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		Intent intent = null;
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	        case R.id.action_command:
-	        	intent = new Intent(this, CommandMenuActivity.class);
-	    	    startActivity(intent);
-	            return true;
-	        case R.id.action_control:
-	        	intent  = new Intent(this, ControlMenuActivity.class);
-	    	    startActivity(intent);
-	            return true;
-	        case R.id.action_converter:
-	        	intent  = new Intent(this, ConverterMenuActivity.class);
-	    	    startActivity(intent);
-	            return true;
-	        case R.id.action_parcel:
-	        	intent  = new Intent(this, ListParcelActivity.class);
-	    	    startActivity(intent);
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}
 
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.action_command: // Command action
+			startActivity(new Intent(this, CommandActivity.class));
+			return true;
+
+		case R.id.action_control: // Control action
+			startActivity(new Intent(this, ControlActivity.class));
+			return true;
+
+		case R.id.action_converter: // Converter action
+			startActivity(new Intent(this, ConverterActivity.class));
+			return true;
+
+		case R.id.action_parcel: // Parcel action
+			startActivity(new Intent(this, ListParcelActivity.class));
+			return true;
+
+		default: // Unknown action
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
